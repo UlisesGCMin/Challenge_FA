88ab7c577dc17c7e986819ca53d6f3befadde973
Odysseo <ugomezc@minsait.com>
2023-08-31 15:54:32 -0600

    VErsion Final

--- README.md
+++ README.md
@@ -8,14 +8,17 @@ git clone https://github.com/UlisesGCMin/Challenge_FA.git
 
 ```
 
-Con el repo clonado deberiamos instalar un ambiente virtual para ahpi instalar las dependencias, este script fue desarrollado sobre ** python 3.10**. Si ya tiene el ambiente virtual activado o lo va a ejecutar en local, ejecutamos las siguientes líneas.
+Con el repo clonado deberiamos instalar un ambiente virtual para ahpi instalar las dependencias, este script fue desarrollado sobre **python 3.10**. Si ya tiene el ambiente virtual activado o lo va a ejecutar en local, ejecutamos las siguientes líneas.
 
 ```py
 
 pip install -r requirements.txt
 python -m spacy download es_core_news_sm
+uvicorn main:app --reload
 
 ```
+
+Accedemos a la siguiente ruta donde podemos hacer pruebas: **http://127.0.0.1:8000/docs#/**
 
 ## Funcionamiento
 
--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from pydantic import BaseModel
 # Para Spacy
 import spacy
 import es_core_news_sm
+import pandas as pd
+
+# Se carga por fuera de la fucnión para que no se re-ejecute cada llamada
 nlp = es_core_news_sm.load()
 
 # Define la clase para la data
@@ -13,14 +16,39 @@ class media_input(BaseModel):
     # Con esto nos aseguramos que el input sea una lista
     oraciones: list[str]
 
+# Definimos la lista con los ejemplos default
+Lista_MSGS = ["Apple está buscando comprar una startup del Reino Unido por mil millones de dólares.",
+                  "San Francisco considera prohibir los robots de entrega en la acera."]
+
+# Definimos el handler para la API
 app = FastAPI()
 
 @app.post("/mensajes")
-def root(msg_input: media_input):
+def root(msg_input: list[str] = Lista_MSGS):
+    """
+    Con esta funcion devolvemos un json con la identifiación de las entidades. \n
+
+    Args: \n
+        - Toma como input un elemento de lista de strings y por defecto una lista de 2 elementos. \n
+
+    Return:  \n
+        - Regresa un JSON con la información separada por llaves. \n 
+    """
     # Capturamos la entrada de mensajes
-    Lista_MSGS = msg_input.oraciones
+    Lista_MSGS = msg_input
     
-    # Para cada oración extraemos entidades
+    object_to_return = {}
+    object_to_return["entidades"] = []
+    object_to_return["oraciones"] = []
 
+    for oraciones in  Lista_MSGS:
+        elementos = nlp(oraciones)
+        object_to_return["oraciones"].append(oraciones)
+        for entities in elementos.ents:
+            # save_ent = [entities.text, entities.label_]
+            value = {entities.text : entities.label_}
+            object_to_return["entidades"].append(value)
+    
+    final_dict = {"resultado": object_to_return}
 
-    return new_elem
+    return final_dict
